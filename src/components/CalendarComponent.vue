10367766fbe122d7240b442b80cf4f9d36f3ce79
Arianny Janse <arianny.janse@gmail.com>
2020-10-08 10:51:23 -0500

    MIN-6 trabajando en componente calendario

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -3,37 +3,8 @@
     <v-col>
       <v-sheet height="64">
         <v-toolbar flat>
-          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">Today</v-btn>
-          <v-btn fab text small color="grey darken-2" @click="prev">
-            <v-icon small>fas fa-chevron-left</v-icon>
-          </v-btn>
-          <v-btn fab text small color="grey darken-2" @click="next">
-            <v-icon small>fas fa-chevron-right</v-icon>
-          </v-btn>
           <v-toolbar-title v-if="$refs.calendar">{{ $refs.calendar.title }}</v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-menu bottom right>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>fas fa-caret-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
         </v-toolbar>
       </v-sheet>
       <v-sheet height="600">
@@ -45,8 +16,7 @@
           :event-color="getEventColor"
           :type="type"
           @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="viewDay"
+          @click:day="newEvent"
           @change="updateRange"
         ></v-calendar>
         <v-menu
@@ -58,22 +28,44 @@
           <v-card color="grey lighten-4" min-width="350px" flat>
             <v-toolbar :color="selectedEvent.color" dark>
               <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+                <v-icon>far fa-edit</v-icon>
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
               <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
+                <v-icon>fas fa-heart</v-icon>
               </v-btn>
               <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
+                <v-icon>fas fa-ellipsis-v</v-icon>
               </v-btn>
             </v-toolbar>
             <v-card-text>
-              <span v-html="selectedEvent.details"></span>
+              <span v-html="selectedEvent"></span>
             </v-card-text>
             <v-card-actions>
               <v-btn text color="secondary" @click="selectedOpen = false">Cancel</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+
+        <v-menu
+          v-model="selectedOpen2"
+          :close-on-content-click="false"
+          position-x="50"
+        >
+          <v-card elevation="2" color="grey lighten-4" min-width="350px" flat>
+            <v-toolbar :color="selectedEvent2.color" dark>
+              <v-btn icon>
+                <v-icon>far fa-edit</v-icon>
+              </v-btn>
+              <v-toolbar-title v-html="selectedEvent2.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <span v-html="selectedEvent2"></span>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="secondary" @click="selectedOpen2 = false">Cancel</v-btn>
             </v-card-actions>
           </v-card>
         </v-menu>
@@ -88,15 +80,13 @@ export default {
   data: () => ({
     focus: "",
     type: "month",
-    typeToLabel: {
-      month: "Month",
-      week: "Week",
-      day: "Day",
-      "4day": "4 Days"
-    },
     selectedEvent: {},
     selectedElement: null,
     selectedOpen: false,
+    selectedEvent2: {},
+    selectedOpen2: false,
+    createEvent: null,
+    createStart: null,
     events: [],
     colors: [
       "blue",
@@ -122,25 +112,14 @@ export default {
     this.$refs.calendar.checkChange();
   },
   methods: {
-    viewDay({ date }) {
-      this.focus = date;
-      this.type = "day";
-    },
     getEventColor(event) {
       return event.color;
-    },
-    setToday() {
-      this.focus = "";
-    },
-    prev() {
-      this.$refs.calendar.prev();
-    },
-    next() {
-      this.$refs.calendar.next();
     },
     showEvent({ nativeEvent, event }) {
       const open = () => {
         this.selectedEvent = event;
+        console.log(nativeEvent);
+        console.log(event);
         this.selectedElement = nativeEvent.target;
         setTimeout(() => {
           this.selectedOpen = true;
@@ -156,15 +135,15 @@ export default {
 
       nativeEvent.stopPropagation();
     },
-    updateRange({ start, end }) {
+    updateRange() {
       const events = [];
 
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
+      //const min = new Date(`${start.date}T00:00:00`);
+      //const max = new Date(`${end.date}T23:59:59`);
+      //const days = (max.getTime() - min.getTime()) / 86400000;
+      //const eventCount = this.rnd(days, days + 20);
 
-      for (let i = 0; i < eventCount; i++) {
+      /**for (let i = 0; i < eventCount; i++) {
         const allDay = this.rnd(0, 3) === 0;
         const firstTimestamp = this.rnd(min.getTime(), max.getTime());
         const first = new Date(firstTimestamp - (firstTimestamp % 900000));
@@ -178,12 +157,47 @@ export default {
           color: this.colors[this.rnd(0, this.colors.length - 1)],
           timed: !allDay
         });
-      }
+      }**/
+
+      const allDay = this.rnd(0, 3) === 0;
+      events.push({
+        name: "Prueba",
+        start: new Date("2020-10-05T18:00:00"),
+        end: new Date("2020-10-05T18:59:59"),
+        color: this.colors[this.rnd(0, this.colors.length - 1)],
+        timed: !allDay
+      });
 
       this.events = events;
     },
     rnd(a, b) {
       return Math.floor((b - a + 1) * Math.random()) + a;
+    },
+    newEvent({ date }) {
+      this.createStart = new Date(`${date}T00:00:00`);
+      this.createEvent = {
+        name: `Solicitud de cotización`,
+        color: this.colors[this.rnd(0, this.colors.length - 1)],
+        start: this.createStart,
+        end: this.createStart,
+        timed: true
+      };
+      //this.events.push(this.createEvent);
+      //console.log(this.createEvent);
+      const open = () => {
+        this.selectedEvent2 = this.createEvent;
+        setTimeout(() => {
+          this.selectedOpen2 = true;
+        }, 10);
+      };
+
+      if (this.selectedOpen2) {
+        this.selectedOpen2 = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+        this.events.push(this.createEvent);
+      }
     }
   }
 };
