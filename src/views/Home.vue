8274e312b6aff358a8919ff2e544e340e922fc71
Arianny Janse <arianny.janse@gmail.com>
2020-10-15 11:47:55 -0500

    MIN-7 agregada la conexion fe - be

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import api from '@/api'
+import api from "@/api";
 
 export default {
   name: "Home",
@@ -18,13 +18,12 @@ export default {
   data: () => ({
     users: []
   }),
+  //Prueba para el llamado a la api, no necesario en la versión final
   created() {
     return api
-        .getUsers()
-        .then(response => (this.users = response))
-        .finally(() => (
-          this.users ? {} : console.log(this.users)
-        ))
+      .getUsers()
+      .then(response => (this.users = response))
+      .finally(() => (console.log(this.users)));
   }
 };
 </script>
