540d31c23a36e0ba6c3977e6441296c374b731db
victorm0612 <rojasvictorvargas@gmail.com>
2020-10-25 17:44:19 -0500

    adding design

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,40 +1,116 @@
 <template>
-  <div>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <h1>Iniciar Sesión</h1>
-          <v-text-field
-            label="Usuario o correo electrónico"
-            :rules="[rules.required, rules.email]"
-            v-model="email"
-            hide-details="auto">
-          </v-text-field>
-          <v-text-field
-            label="Contraseña"
-            :rules="[rules.required, rules.password]"
-            :append-icon="show ? 'fas fa-eye' : 'fas fa-eye-slash'"
-            v-model="password"
-            :type="show ? 'text' : 'password'"
-            @click:append="show = !show">
-          </v-text-field>
-          <v-btn text color="primary" @click="getUser">Iniciar Sesión</v-btn>
-          <h4>¿No estás registrado?</h4>
-          <router-link text color="primary" to="register">Registrarse</router-link>
-        </v-col>
-        <v-col cols="12">
-          <v-btn text color="primary">Ingresar como Empleado</v-btn>
-          <v-btn text color="primary">Ingresar como Administrador</v-btn>
-          <v-alert class="mt-3" 
-              :value="show_credentials"
-              :type="alert ? 'success' : 'error'"
-              >{{message}}
-          </v-alert>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container class="fill-height" fluid>
+    <v-row class="d-flex" align="center" justify="center">
+      <v-col cols="12" sm="5" md="4">
+        <v-card class="elevation-12 __b-20">
+          <v-card-text>
+            <v-container fluid>
+              <v-row>
+                <v-col cols="12" class="text-center">
+                  <h3 class="headline">
+                    Iniciar Sesión
+                  </h3>
+                  <h4 class="subtitle-1 mb-3">
+                    ¡Bienvenido {{message_user}}!
+                  </h4>
+                </v-col>
+              </v-row>
+              <v-row align="center">
+                <v-col cols="12" class="aling-center">
+                  <v-form>
+                    <v-text-field
+                    label="correo electrónico"
+                    type="email"
+                    outlined dense
+                    :rules="[rules.required, rules.email]"
+                    v-model="email">
+                    </v-text-field>
+                    <v-text-field
+                    label="Contraseña"
+                    outlined dense
+                    :rules="[rules.required, rules.password]"
+                    :append-icon="show ? 'fas fa-eye' : 'fas fa-eye-slash'"
+                    v-model="password"
+                    :type="show ? 'text' : 'password'"
+                    @click:append="show = !show">
+                    </v-text-field>
+                    <div class="d-flex">
+                      <v-btn text color="primary" class="text-none px-2 __btn-login-text" @click="moveToRegister">
+                        Crear cuenta
+                      </v-btn>
+                      <v-spacer/>
+                      <v-btn color="primary" class="text-none px-2" @click="getUser">
+                        Iniciar Sesión
+                      </v-btn>
+                    </div>
+                  </v-form>
+                </v-col>
+              </v-row>
+              <v-row align="center">
+                <v-col>
+                  <v-radio-group>
+                    <v-radio
+                    v-for="item in typeUser"
+                    :key="item"
+                    @click="message_user=item"
+                    :label="`Ingresar como ${item}`"
+                    :value="item"
+                    ></v-radio>
+                    </v-radio-group>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+        </v-card>
+        <v-alert class="mt-3" 
+        :value="show_credentials"
+        :type="alert ? 'success' : 'error'">
+        {{message_login}}
+        </v-alert>
+      </v-col>
+    </v-row>
+    
+  </v-container>
 </template>
+
+ <!---<template>
+
+ <div>
+    <v-container>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="3">
+            <h1>Iniciar Sesión</h1>
+            <v-text-field
+              label="Usuario o correo electrónico"
+              :rules="[rules.required, rules.email]"
+              v-model="email"
+              hide-details="auto">
+            </v-text-field>
+            <v-text-field
+              label="Contraseña"
+              :rules="[rules.required, rules.password]"
+              :append-icon="show ? 'fas fa-eye' : 'fas fa-eye-slash'"
+              v-model="password"
+              :type="show ? 'text' : 'password'"
+              @click:append="show = !show">
+            </v-text-field>
+            <v-btn text color="primary" @click="getUser">Iniciar Sesión</v-btn>
+            <h4>¿No estás registrado?</h4>
+            <router-link text color="primary" to="register">Registrarse</router-link>
+          </v-col>
+          <v-col cols="12">
+            <v-btn text color="primary">Ingresar como Empleado</v-btn>
+            <v-btn text color="primary">Ingresar como Administrador</v-btn>
+            <v-alert class="mt-3" 
+                :value="show_credentials"
+                :type="alert ? 'success' : 'error'"
+                >{{message}}
+            </v-alert>
+          </v-col>
+        </v-row>
+    </v-container>
+  </div> 
+</template>--->
 
 <script>
 import axios from 'axios';
@@ -42,8 +118,10 @@ export default {
   name: "LoginComponent",
   data() {
     return {
+      typeUser:['Administrador','Empleado','Cliente'],
       users:[],
-      message: "",
+      message_login: "",
+      message_user: "",
       step: 1,
       email: "",
       password: "",
@@ -74,17 +152,20 @@ export default {
       evt.preventDefault();
       for(let user of this.users){
         if(user.email == this.email && user.password == this.password){
-          this.message="Loading..."
+          this.message_login="Loading..."
           this.alert= true
           setTimeout(() => { this.$router.push({ name: "ClientMain" }); }, 1500);
           break;
         }
         else{
-          this.message="El correo o la contraseña ingresada no son correctas."
+          this.message_login="El correo o la contraseña ingresada no son correctas."
           this.alert= false
         }
       }
       this.show_credentials = true;
+    },
+    moveToRegister: function() {
+      this.$router.push({ name: "Register" });
     }
   },
 };
