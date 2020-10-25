e5232cf8c90c5151ffa7d764c7ecbed76317d4f5
Arianny Janse <arianny.janse@gmail.com>
2020-10-24 19:58:48 -0500

    MIN-10 agregada funcionalidad para editar el perfil del usuario

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -2,32 +2,92 @@
   <v-container fluid>
     <v-layout column>
       <v-card>
+        <v-card-title class="justify-center">{{
+          form.firstName + " " + form.lastName
+        }}</v-card-title>
         <v-card-text>
           <v-flex class="mb-4">
             <v-avatar size="96" class="mr-4">
               <img
-                :src="require('@/assets/avatars/avatar_' + form.avatar + '.png')"
+                :src="
+                  require('@/assets/avatars/avatar_' + form.avatar + '.png')
+                "
                 alt="Avatar"
               />
             </v-avatar>
-            <v-btn @click="openAvatarPicker">Change Avatar</v-btn>
           </v-flex>
-          <v-text-field
-            v-model="form.firstName"
-            label="FirstName"
-          ></v-text-field>
-          <v-text-field
-            v-model="form.lastName"
-            label="Last Name"
-          ></v-text-field>
-          <v-text-field
-            v-model="form.contactEmail"
-            label="Email Address"
-          ></v-text-field>
+          <v-btn v-if="editProfile" @click="openAvatarPicker">Cambiar Avatar</v-btn>
+          <v-row class="justify-center">
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.firstName"
+                label="Nombre"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.lastName"
+                label="Apellido"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="justify-center">
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.email"
+                label="Correo electrónico"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.phone"
+                label="Número de celular"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="justify-center">
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.documentType"
+                label="Tipo de documento"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.documentNumber"
+                label="Número de documento"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row class="justify-center">
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.address"
+                label="Dirección"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" md="4">
+              <v-text-field
+                :disabled="!editProfile"
+                v-model="form.gender"
+                label="Género"
+              ></v-text-field>
+            </v-col>
+          </v-row>
         </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" :loading="loading">
-            Save Changes
+        <v-card-actions class="justify-center">
+          <v-btn color="primary" :loading="loading" @click="edit_profile">
+            {{ btnTitle }}
+          </v-btn>
+          <v-btn color="primary" :disabled="editProfile">
+            Ver mis citas
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -51,10 +111,17 @@ export default {
       form: {
         firstName: "John",
         lastName: "Doe",
-        contactEmail: "john@doe.com",
+        email: "john@doe.com",
+        phone: 3012222222,
+        documentType: "Cédula de Ciudadania",
+        documentNumber: 123456789,
+        address: "some address",
+        gender: "Masculino",
         avatar: "MALE_CAUCASIAN_BLACK_BEARD"
       },
-      showAvatarPicker: false
+      showAvatarPicker: false,
+      btnTitle: "Editar Perfil",
+      editProfile: false
     };
   },
   methods: {
@@ -63,6 +130,14 @@ export default {
     },
     selectAvatar(avatar) {
       this.form.avatar = avatar;
+    },
+    edit_profile() {
+      this.editProfile = !this.editProfile;
+      if (this.editProfile) {
+        this.btnTitle = "Guardar Cambios"
+      } else {
+        this.btnTitle = "Editar Perfil"
+      }
     }
   }
 };
