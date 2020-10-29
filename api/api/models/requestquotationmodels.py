360ec58bafac1687ca8e70c05d381c9c0cf7cdb2
jose0926 <jose.martinez.castillo@correounivalle.edu.co>
2020-10-28 23:50:44 -0500

    MIN-17 Se agrega opción para obtener todas las citas de un usuario

--- api/api/models/requestquotationmodels.py
+++ api/api/models/requestquotationmodels.py
@@ -1,23 +1,19 @@
 from django.db import models
+from api.models.usermodels import User
 
 
-# Create your models here.
 class RequestQuotation(models.Model):
     RANGE_1 = "8:00 - 9:30"
     RANGE_2 = "10:00 - 11:30"
     RANGE_3 = "13:00 - 14:30"
     RANGE_4 = "15:00 - 16:30"
     RANGE_5 = "17:00 - 18:30"
-
     RANGES = ((RANGE_1, 1), (RANGE_2, 2), (RANGE_3, 3), (RANGE_4, 4), (RANGE_5, 5))
-    # La @request_date se autogenera al momento de pedir la cita
-    request_date = models.DateTimeField(auto_now_add=True)
 
-    # la @scheduled_date es la que el cliente debe asignarle
-    # Formato: YYYY-MM-DD
+    request_date = models.DateTimeField(auto_now_add=True)
     scheduled_date = models.DateField()
     time_range = models.TextField(max_length=13,choices=RANGES)
     approved = models.BooleanField()
     service_type = models.TextField(max_length=50)
     description = models.TextField(max_length=200)
-    fkUser_int = models.IntegerField()
+    fkUser_id = models.ForeignKey(User, related_name='User_id', on_delete=models.CASCADE)
--- api/api/serializers/requestquotationserializer.py
+++ api/api/serializers/requestquotationserializer.py
@@ -5,12 +5,14 @@ from api.models.requestquotationmodels import RequestQuotation
 class RequestQuotationSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
-        fechaUsuario = data.get('scheduled_date')
-        rangoUsuario = data.get('time_range')
-        ocupado = RequestQuotation.objects.filter(scheduled_date=fechaUsuario, time_range=rangoUsuario)
 
-        if ocupado:
-            raise serializers.ValidationError({"scheduled_date and time_range": "Ese rango de hora en esa fecha está ocupado"})
+        if data != None:
+            fechaUsuario = data.get('scheduled_date')
+            rangoUsuario = data.get('time_range')
+            ocupado = RequestQuotation.objects.filter(scheduled_date=fechaUsuario, time_range=rangoUsuario)
+            if ocupado:
+                raise serializers.ValidationError({"scheduled_date and time_range": "Ese rango de hora en esa fecha está ocupado"})
+            return data
         return data
 
     class Meta:
