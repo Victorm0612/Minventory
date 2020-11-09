3695f0059980585925239285e362ffda62b3e04b
Arianny Janse <53880590+AriannyJanse@users.noreply.github.com>
2020-11-09 15:25:28 -0500

    Update taskmodels.py

--- api/api/models/taskmodels.py
+++ api/api/models/taskmodels.py
@@ -7,6 +7,6 @@ from api.models.usermodels import User
 class Task(models.Model):
     approved_date = models.DateTimeField(auto_now_add=True)
     realization_date = models.DateTimeField()
-    fkAssignment_worker = models.ForeignKey(User, on_delete=models.CASCADE)
+    fkAssignment_worker = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     fkElement_id = models.ForeignKey(Element, related_name='Element_id', on_delete=models.CASCADE)
     fkTask_status = models.ForeignKey(Status, related_name='Status_id', on_delete=models.CASCADE)
