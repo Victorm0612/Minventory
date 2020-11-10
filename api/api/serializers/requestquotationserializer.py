876619c0357a1657cebb13ea778299236055f86d
JPablo9605 <70659132+JPablo9605@users.noreply.github.com>
2020-11-10 15:53:22 -0500

    PR corrección quotation
    Se corrigió error de duplicado al tratar de modificar una cita

--- api/api/serializers/requestquotationserializer.py
+++ api/api/serializers/requestquotationserializer.py
@@ -9,9 +9,15 @@ class RequestQuotationSerializer(serializers.ModelSerializer):
         if data != None:
             fechaUsuario = data.get('scheduled_date')
             rangoUsuario = data.get('time_range')
+            idUsuario = data.get('fkUser_id')
             ocupado = RequestQuotation.objects.filter(scheduled_date=fechaUsuario, time_range=rangoUsuario)
-            if ocupado:
+            modificar = RequestQuotation.objects.filter(fkUser_id=idUsuario)
+            if modificar:
+                return data
+
+            elif ocupado:
                 raise serializers.ValidationError({"scheduled_date_and_time_range": "Ese rango de hora en esa fecha está ocupado"})
+
             return data
 
     class Meta:
