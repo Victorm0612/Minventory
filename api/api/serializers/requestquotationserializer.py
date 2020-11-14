2794fa25c820aeb717c463e5772f5aef9b00f273
Arianny Janse <arianny.janse@gmail.com>
2020-11-13 23:09:56 -0500

    MIN-41 eliminando comentarios del serializer de requestquotation

--- api/api/serializers/requestquotationserializer.py
+++ api/api/serializers/requestquotationserializer.py
@@ -28,36 +28,6 @@ class RequestQuotationPutSerializer(serializers.ModelSerializer):
         if data != None:
             print(data)
 
-
-
-    # def validate(self, data):
-
-    #     if data != None:
-    #         fechaUsuario = data.get('scheduled_date')
-    #         rangoUsuario = data.get('time_range')
-    #         idUsuario = data.get('fkUser_id')
-    #         idQuotation = data.get('pk')
-    #         # print("idQuotation: ", idQuotation)
-    #         valid_user = RequestQuotation.objects.filter(
-    #             scheduled_date=fechaUsuario, time_range=rangoUsuario, fkUser_id=idUsuario)
-    #         ocupado = RequestQuotation.objects.filter(id=idQuotation, scheduled_date=fechaUsuario, time_range=rangoUsuario)
-    #         # verificarUser = ocupado.exclude(
-    #         #     scheduled_date=fechaUsuario, time_range=rangoUsuario, fkUser_id=idUsuario)
-
-    #         if valid_user:
-    #             # print("verificarUser: ", verificarUser)
-    #             # print("ocupado: ", ocupado)
-    #             if ocupado:
-
-    #                 raise serializers.ValidationError(
-    #                     {"scheduled_date_and_time_range": "Ese rango de hora en esa fecha está ocupado"})
-    #             return data
-
-    #         elif ocupado:
-    #             raise serializers.ValidationError(
-    #                 {"scheduled_date_and_time_range": "Ese rango de hora en esa fecha está ocupado"})
-    #         return data
-
     class Meta:
         model = RequestQuotation
         fields = '__all__'
