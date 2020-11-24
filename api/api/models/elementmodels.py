44abfc6f34ad472dd2f1cc77181e3c6de0c1a0ec
JPablo9605 <70659132+JPablo9605@users.noreply.github.com>
2020-11-24 12:46:19 -0500

    pr-fkElement-Task
    Se añadió fk de task en element, lo cual liga los elementos con una tarea en especifico.

--- api/api/models/elementmodels.py
+++ api/api/models/elementmodels.py
@@ -1,5 +1,7 @@
 from django.db import models
 from api.models.inventorymodels import Inventory
+from api.models.taskmodels import Task
+
 
 
 class Element(models.Model):
@@ -9,3 +11,4 @@ class Element(models.Model):
     reseller = models.TextField(max_length=200)
     element_stock = models.IntegerField()
     fkInventory_id = models.ForeignKey(Inventory, related_name='Inventory_id', on_delete=models.CASCADE, default=0)
+    fkTask_id = models.ForeignKey(Task, related_name='Task_id', on_delete=models.CASCADE, null=True)
